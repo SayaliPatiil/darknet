56d8509df5b7a42a5cad145bc2cffaa13c9aff14
SayaliPatiil <92857276+SayaliPatiil@users.noreply.github.com>
2022-08-25 12:58:07 +0530

    Update yolo.c

--- examples/yolo.c
+++ examples/yolo.c
@@ -14,7 +14,7 @@ void train_yolo(char *cfgfile, char *weightfile)
     printf("Learning Rate: %g, Momentum: %g, Decay: %g\n", net->learning_rate, net->momentum, net->decay);
     int imgs = net->batch*net->subdivisions;
     int i = *net->seen/imgs;
-    data train, buffer;
+    
 
 
     layer l = net->layers[net->n - 1];
